8a0dad646671f37d37f116b64f56361702c188ba
WR104 <252600486L@gmail.com>
2022-09-05 02:01:58 -0700

    change some files

--- EventBasics/app.js
+++ EventBasics/app.js
@@ -4,3 +4,18 @@ btn.onclick = function(){
     console.log("You clicked me");
     console.log("I hope it worked");
 }
+
+document.querySelector('h1').onclick = () => {
+    alert('you clicked the h1!')
+}
+ const btn3 = document.querySelector('#v3');
+ btn3.addEventListener('dbclick',() => {alert("clicked v3")});
+
+ const input = document.querySelector('input');
+ input.addEventListener('keydown',() => {
+    console.log('keydown');
+ })
+
+ window.addEventListener('keydown',(e) => {
+    console.log(e.code);
+ })
