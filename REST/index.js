c64273ff6a850b5c662e56fdf6631d938456a6cc
WR104 <252600486L@gmail.com>
2022-09-12 02:04:59 -0700

    add REST

---
+++ REST/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/tacos', (req, res) => {
+    res.send("Get /tacos response");
+})
+
+app.post('/tacos',(req,res)=>{
+    res.send("Post /tacos response")
+})
+app.listen(3000, () => {
+    console.log("On port 3000")
+})
