ab04294f7bdc0f31e50e3c59e72205f3bb7c2bc6
WR104 <252600486L@gmail.com>
2022-09-19 01:39:13 -0700

    add REST

--- REST/index.js
+++ REST/index.js
@@ -1,13 +1,54 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/tacos', (req, res) => {
-    res.send("Get /tacos response");
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        username: 'Tood',
+        comment: 'Lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd',
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', {comments})
+})
+app.get('/comments/new', (req,res)=>{
+    res.render('comments/new')
+})
+app.post('/comments',(req,res)=>{
+    const {username, comment} = req.body;
+    comments.push({username,comment})
+    res.redirect('/comments')
 })
 
-app.post('/tacos',(req,res)=>{
-    res.send("Post /tacos response")
+app.get('/tacos', (req, res) => {
+    res.send("Get /tacos response")
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body
+    res.send(`There are ${qty} ${meat}`)
 })
 app.listen(3000, () => {
     console.log("On port 3000")
 })
+
+
+
